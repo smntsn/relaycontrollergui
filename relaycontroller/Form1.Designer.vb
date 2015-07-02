32c7d846a1f9f7aa7e96ca4a5d2583bd25821113
smntsn <unconfigured@null.unconfigured.org.tr>
2015-07-03 00:37:30 +0300

    Sends short commands to an Arduino with ethernet connection and 4 Relays.
    Signed-off-by: smntsn <unconfigured@null.unconfigured.org.tr>

---
+++ relaycontroller/Form1.Designer.vb
@@ -0,0 +1,503 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button4off = New System.Windows.Forms.Button()
+        Me.Button4on = New System.Windows.Forms.Button()
+        Me.Button3off = New System.Windows.Forms.Button()
+        Me.Button3on = New System.Windows.Forms.Button()
+        Me.ButtonAllOff = New System.Windows.Forms.Button()
+        Me.Button2off = New System.Windows.Forms.Button()
+        Me.ButtonAllOn = New System.Windows.Forms.Button()
+        Me.Button2on = New System.Windows.Forms.Button()
+        Me.Button1off = New System.Windows.Forms.Button()
+        Me.Button1on = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer1r = New System.Windows.Forms.Timer(Me.components)
+        Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
+        Me.WebBrowser3 = New System.Windows.Forms.WebBrowser()
+        Me.WebBrowser4 = New System.Windows.Forms.WebBrowser()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Timer2r = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3r = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4r = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.NumericUpDown7 = New System.Windows.Forms.NumericUpDown()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.NumericUpDown8 = New System.Windows.Forms.NumericUpDown()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'WebBrowser1
+        '
+        resources.ApplyResources(Me.WebBrowser1, "WebBrowser1")
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Button4off)
+        Me.GroupBox1.Controls.Add(Me.Button4on)
+        Me.GroupBox1.Controls.Add(Me.Button3off)
+        Me.GroupBox1.Controls.Add(Me.Button3on)
+        Me.GroupBox1.Controls.Add(Me.ButtonAllOff)
+        Me.GroupBox1.Controls.Add(Me.Button2off)
+        Me.GroupBox1.Controls.Add(Me.ButtonAllOn)
+        Me.GroupBox1.Controls.Add(Me.Button2on)
+        Me.GroupBox1.Controls.Add(Me.Button1off)
+        Me.GroupBox1.Controls.Add(Me.Button1on)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.TabStop = False
+        '
+        'Button4off
+        '
+        resources.ApplyResources(Me.Button4off, "Button4off")
+        Me.Button4off.Name = "Button4off"
+        Me.Button4off.UseVisualStyleBackColor = True
+        '
+        'Button4on
+        '
+        resources.ApplyResources(Me.Button4on, "Button4on")
+        Me.Button4on.Name = "Button4on"
+        Me.Button4on.UseVisualStyleBackColor = True
+        '
+        'Button3off
+        '
+        resources.ApplyResources(Me.Button3off, "Button3off")
+        Me.Button3off.Name = "Button3off"
+        Me.Button3off.UseVisualStyleBackColor = True
+        '
+        'Button3on
+        '
+        resources.ApplyResources(Me.Button3on, "Button3on")
+        Me.Button3on.Name = "Button3on"
+        Me.Button3on.UseVisualStyleBackColor = True
+        '
+        'ButtonAllOff
+        '
+        resources.ApplyResources(Me.ButtonAllOff, "ButtonAllOff")
+        Me.ButtonAllOff.Name = "ButtonAllOff"
+        Me.ButtonAllOff.UseVisualStyleBackColor = True
+        '
+        'Button2off
+        '
+        resources.ApplyResources(Me.Button2off, "Button2off")
+        Me.Button2off.Name = "Button2off"
+        Me.Button2off.UseVisualStyleBackColor = True
+        '
+        'ButtonAllOn
+        '
+        resources.ApplyResources(Me.ButtonAllOn, "ButtonAllOn")
+        Me.ButtonAllOn.Name = "ButtonAllOn"
+        Me.ButtonAllOn.UseVisualStyleBackColor = True
+        '
+        'Button2on
+        '
+        resources.ApplyResources(Me.Button2on, "Button2on")
+        Me.Button2on.Name = "Button2on"
+        Me.Button2on.UseVisualStyleBackColor = True
+        '
+        'Button1off
+        '
+        resources.ApplyResources(Me.Button1off, "Button1off")
+        Me.Button1off.Name = "Button1off"
+        Me.Button1off.UseVisualStyleBackColor = True
+        '
+        'Button1on
+        '
+        resources.ApplyResources(Me.Button1on, "Button1on")
+        Me.Button1on.Name = "Button1on"
+        Me.Button1on.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2000
+        '
+        'Timer1r
+        '
+        Me.Timer1r.Interval = 300
+        '
+        'WebBrowser2
+        '
+        resources.ApplyResources(Me.WebBrowser2, "WebBrowser2")
+        Me.WebBrowser2.Name = "WebBrowser2"
+        Me.WebBrowser2.ScrollBarsEnabled = False
+        Me.WebBrowser2.WebBrowserShortcutsEnabled = False
+        '
+        'WebBrowser3
+        '
+        resources.ApplyResources(Me.WebBrowser3, "WebBrowser3")
+        Me.WebBrowser3.Name = "WebBrowser3"
+        Me.WebBrowser3.ScrollBarsEnabled = False
+        Me.WebBrowser3.WebBrowserShortcutsEnabled = False
+        '
+        'WebBrowser4
+        '
+        resources.ApplyResources(Me.WebBrowser4, "WebBrowser4")
+        Me.WebBrowser4.Name = "WebBrowser4"
+        Me.WebBrowser4.ScrollBarsEnabled = False
+        Me.WebBrowser4.WebBrowserShortcutsEnabled = False
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'Timer2r
+        '
+        Me.Timer2r.Interval = 300
+        '
+        'Timer3r
+        '
+        Me.Timer3r.Interval = 300
+        '
+        'Timer4r
+        '
+        Me.Timer4r.Interval = 300
+        '
+        'Label7
+        '
+        resources.ApplyResources(Me.Label7, "Label7")
+        Me.Label7.Name = "Label7"
+        '
+        'Label8
+        '
+        resources.ApplyResources(Me.Label8, "Label8")
+        Me.Label8.Name = "Label8"
+        '
+        'Label10
+        '
+        resources.ApplyResources(Me.Label10, "Label10")
+        Me.Label10.Name = "Label10"
+        '
+        'Label11
+        '
+        resources.ApplyResources(Me.Label11, "Label11")
+        Me.Label11.Name = "Label11"
+        '
+        'Label12
+        '
+        resources.ApplyResources(Me.Label12, "Label12")
+        Me.Label12.Name = "Label12"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Controls.Add(Me.Label15)
+        Me.GroupBox2.Controls.Add(Me.NumericUpDown4)
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.NumericUpDown3)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.NumericUpDown2)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.NumericUpDown1)
+        resources.ApplyResources(Me.GroupBox2, "GroupBox2")
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.TabStop = False
+        '
+        'NumericUpDown1
+        '
+        resources.ApplyResources(Me.NumericUpDown1, "NumericUpDown1")
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        '
+        'Label9
+        '
+        resources.ApplyResources(Me.Label9, "Label9")
+        Me.Label9.Name = "Label9"
+        '
+        'Label13
+        '
+        resources.ApplyResources(Me.Label13, "Label13")
+        Me.Label13.Name = "Label13"
+        '
+        'NumericUpDown2
+        '
+        resources.ApplyResources(Me.NumericUpDown2, "NumericUpDown2")
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        '
+        'Label14
+        '
+        resources.ApplyResources(Me.Label14, "Label14")
+        Me.Label14.Name = "Label14"
+        '
+        'NumericUpDown3
+        '
+        resources.ApplyResources(Me.NumericUpDown3, "NumericUpDown3")
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        '
+        'Label15
+        '
+        resources.ApplyResources(Me.Label15, "Label15")
+        Me.Label15.Name = "Label15"
+        '
+        'NumericUpDown4
+        '
+        resources.ApplyResources(Me.NumericUpDown4, "NumericUpDown4")
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.CheckBox1)
+        Me.GroupBox3.Controls.Add(Me.Label16)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown5)
+        Me.GroupBox3.Controls.Add(Me.Label17)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown6)
+        Me.GroupBox3.Controls.Add(Me.Label18)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown7)
+        Me.GroupBox3.Controls.Add(Me.Label19)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown8)
+        resources.ApplyResources(Me.GroupBox3, "GroupBox3")
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.TabStop = False
+        '
+        'Label16
+        '
+        resources.ApplyResources(Me.Label16, "Label16")
+        Me.Label16.Name = "Label16"
+        '
+        'NumericUpDown5
+        '
+        resources.ApplyResources(Me.NumericUpDown5, "NumericUpDown5")
+        Me.NumericUpDown5.Name = "NumericUpDown5"
+        '
+        'Label17
+        '
+        resources.ApplyResources(Me.Label17, "Label17")
+        Me.Label17.Name = "Label17"
+        '
+        'NumericUpDown6
+        '
+        resources.ApplyResources(Me.NumericUpDown6, "NumericUpDown6")
+        Me.NumericUpDown6.Name = "NumericUpDown6"
+        '
+        'Label18
+        '
+        resources.ApplyResources(Me.Label18, "Label18")
+        Me.Label18.Name = "Label18"
+        '
+        'NumericUpDown7
+        '
+        resources.ApplyResources(Me.NumericUpDown7, "NumericUpDown7")
+        Me.NumericUpDown7.Name = "NumericUpDown7"
+        '
+        'Label19
+        '
+        resources.ApplyResources(Me.Label19, "Label19")
+        Me.Label19.Name = "Label19"
+        '
+        'NumericUpDown8
+        '
+        resources.ApplyResources(Me.NumericUpDown8, "NumericUpDown8")
+        Me.NumericUpDown8.Name = "NumericUpDown8"
+        '
+        'CheckBox1
+        '
+        resources.ApplyResources(Me.CheckBox1, "CheckBox1")
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser4)
+        Me.Controls.Add(Me.WebBrowser3)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button4off As Button
+    Friend WithEvents Button4on As Button
+    Friend WithEvents Button3off As Button
+    Friend WithEvents Button3on As Button
+    Friend WithEvents Button2off As Button
+    Friend WithEvents Button2on As Button
+    Friend WithEvents Button1off As Button
+    Friend WithEvents Button1on As Button
+    Friend WithEvents ButtonAllOn As Button
+    Friend WithEvents ButtonAllOff As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer1r As Timer
+    Friend WithEvents WebBrowser2 As WebBrowser
+    Friend WithEvents WebBrowser3 As WebBrowser
+    Friend WithEvents WebBrowser4 As WebBrowser
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer2r As Timer
+    Friend WithEvents Timer3r As Timer
+    Friend WithEvents Timer4r As Timer
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents NumericUpDown5 As NumericUpDown
+    Friend WithEvents Label17 As Label
+    Friend WithEvents NumericUpDown6 As NumericUpDown
+    Friend WithEvents Label18 As Label
+    Friend WithEvents NumericUpDown7 As NumericUpDown
+    Friend WithEvents Label19 As Label
+    Friend WithEvents NumericUpDown8 As NumericUpDown
+    Friend WithEvents Label15 As Label
+    Friend WithEvents NumericUpDown4 As NumericUpDown
+    Friend WithEvents Label14 As Label
+    Friend WithEvents NumericUpDown3 As NumericUpDown
+    Friend WithEvents Label13 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label9 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ relaycontroller/Form1.vb
@@ -0,0 +1,223 @@
+﻿' (c)2015 smntsn. Simple, dirty, cheap code for my central power management system.
+' 4 Relays and an Arduino controls home electricity over ethernet.
+'Arduino has its own code
+
+'Alpha stage!
+
+Public Class Form1
+
+    Dim ArduinoAdr As String
+    Dim ON1 As String
+    Dim ON2 As String
+    Dim ON3 As String
+    Dim ON4 As String
+    Dim OFF1 As String
+    Dim OFF2 As String
+    Dim OFF3 As String
+    Dim OFF4 As String
+    Dim TimerRelayStatus As String
+
+
+    Public Class GlobalVariables
+        'This area will be implemented in near future. Maybe maybe not ! :/
+        'Public Shared UserName As String = "smntsn"
+        'Public Shared UserAge As Integer = 99    
+
+        ''I Hate Using Global Variables. I Hate Easy Way! :P
+    End Class
+
+
+    Private Sub Button1on_Click(sender As Object, e As EventArgs) Handles Button1on.Click
+        ON1 = "http://" & ArduinoAdr & "/?ON1"
+        WebBrowser1.Navigate(ON1)
+        Button1on.Enabled = False
+        Button1off.Enabled = False
+        Timer1.Enabled = True
+        Label2.ForeColor = Color.Green
+
+    End Sub
+
+    Private Sub Button1off_Click(sender As Object, e As EventArgs) Handles Button1off.Click
+        OFF1 = "http://" & ArduinoAdr & "/?OFF1"
+        WebBrowser1.Navigate(OFF1)
+        Button1on.Enabled = False
+        Button1off.Enabled = False
+        Timer1.Enabled = True
+        Label2.ForeColor = Color.Red
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        ArduinoAdr = TextBox1.Text
+
+    End Sub
+
+    Private Sub Button2on_Click(sender As Object, e As EventArgs) Handles Button2on.Click
+        ON2 = "http://" & ArduinoAdr & "/?ON2"
+        WebBrowser2.Navigate(ON2)
+
+        Button2on.Enabled = False
+        Button2off.Enabled = False
+        Timer1.Enabled = True
+        Label3.ForeColor = Color.Green
+
+    End Sub
+
+    Private Sub Button2off_Click(sender As Object, e As EventArgs) Handles Button2off.Click
+        OFF2 = "http://" & ArduinoAdr & "/?OFF2"
+        WebBrowser2.Navigate(OFF2)
+        Button2on.Enabled = False
+        Button2off.Enabled = False
+        Timer1.Enabled = True
+        Label3.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Button3on_Click(sender As Object, e As EventArgs) Handles Button3on.Click
+        ON3 = "http://" & ArduinoAdr & "/?ON3"
+        WebBrowser3.Navigate(ON3)
+        Button3on.Enabled = False
+        Button3off.Enabled = False
+        Timer1.Enabled = True
+        Label4.ForeColor = Color.Green
+
+    End Sub
+
+    Private Sub Button3off_Click(sender As Object, e As EventArgs) Handles Button3off.Click
+        OFF3 = "http://" & ArduinoAdr & "/?OFF3"
+        WebBrowser3.Navigate(OFF3)
+        Button3on.Enabled = False
+        Button3off.Enabled = False
+        Timer1.Enabled = True
+        Label4.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Button4on_Click(sender As Object, e As EventArgs) Handles Button4on.Click
+        ON4 = "http://" & ArduinoAdr & "/?ON4"
+        WebBrowser4.Navigate(ON4)
+        Button4on.Enabled = False
+        Button4off.Enabled = False
+        Timer1.Enabled = True
+        Label5.ForeColor = Color.Green
+
+    End Sub
+
+    Private Sub Button4off_Click(sender As Object, e As EventArgs) Handles Button4off.Click
+        OFF4 = "http://" & ArduinoAdr & "/?OFF4"
+        WebBrowser4.Navigate(OFF4)
+        Button4on.Enabled = False
+        Button4off.Enabled = False
+        Timer1.Enabled = True
+        Label5.ForeColor = Color.Red
+    End Sub
+
+    Private Sub ButtonAllOn_Click(sender As Object, e As EventArgs) Handles ButtonAllOn.Click
+        ON1 = "http://" & ArduinoAdr & "/?ON1"
+        ON2 = "http://" & ArduinoAdr & "/?ON2"
+        ON3 = "http://" & ArduinoAdr & "/?ON3"
+        ON4 = "http://" & ArduinoAdr & "/?ON4"
+        TimerRelayStatus = "makeOn"
+        Timer1r.Enabled = True
+
+
+    End Sub
+
+    Private Sub ButtonAllOff_Click(sender As Object, e As EventArgs) Handles ButtonAllOff.Click
+        OFF1 = "http://" & ArduinoAdr & "/?OFF1"
+        OFF2 = "http://" & ArduinoAdr & "/?OFF2"
+        OFF3 = "http://" & ArduinoAdr & "/?OFF3"
+        OFF4 = "http://" & ArduinoAdr & "/?OFF4"
+        TimerRelayStatus = "makeOff"
+        Timer1r.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick, MyBase.Load
+        Button1on.Enabled = True
+        Button1off.Enabled = True
+        Button2on.Enabled = True
+        Button2off.Enabled = True
+        Button3on.Enabled = True
+        Button3off.Enabled = True
+        Button4on.Enabled = True
+        Button4off.Enabled = True
+        Timer1.Enabled = False
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer1r.Tick
+        If TimerRelayStatus = "makeOn" Then
+            WebBrowser1.Navigate(ON1)
+            Label2.ForeColor = Color.Green
+        Else
+            WebBrowser1.Navigate(OFF1)
+            Label2.ForeColor = Color.Red
+        End If
+        Timer2r.Enabled = True
+        Timer1r.Enabled = False
+    End Sub
+
+    Private Sub Timer2r_Tick(sender As Object, e As EventArgs) Handles Timer2r.Tick
+        If TimerRelayStatus = "makeOn" Then
+            WebBrowser2.Navigate(ON2)
+            Label3.ForeColor = Color.Green
+        Else
+            WebBrowser2.Navigate(OFF2)
+            Label3.ForeColor = Color.Red
+        End If
+
+
+        Timer2r.Enabled = False
+            Timer3r.Enabled = True
+    End Sub
+
+    Private Sub Timer3r_Tick(sender As Object, e As EventArgs) Handles Timer3r.Tick
+        If TimerRelayStatus = "makeOn" Then
+            WebBrowser3.Navigate(ON3)
+            Label4.ForeColor = Color.Green
+        Else
+            WebBrowser3.Navigate(OFF3)
+            Label4.ForeColor = Color.Red
+        End If
+        Timer3r.Enabled = False
+        Timer4r.Enabled = True
+    End Sub
+
+    Private Sub Timer4r_Tick(sender As Object, e As EventArgs) Handles Timer4r.Tick
+        If TimerRelayStatus = "makeOn" Then
+            WebBrowser4.Navigate(ON4)
+            Label5.ForeColor = Color.Green
+        Else
+            WebBrowser4.Navigate(OFF4)
+            Label5.ForeColor = Color.Red
+        End If
+        Timer4r.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate("http://" & ArduinoAdr)
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            NumericUpDown8.Enabled = True
+            NumericUpDown7.Enabled = True
+            NumericUpDown6.Enabled = True
+            NumericUpDown5.Enabled = True
+            Label19.Enabled = True
+            Label18.Enabled = True
+            Label17.Enabled = True
+            Label16.Enabled = True
+        Else
+            NumericUpDown8.Enabled = False
+            NumericUpDown7.Enabled = False
+            NumericUpDown6.Enabled = False
+            NumericUpDown5.Enabled = False
+            Label19.Enabled = False
+            Label18.Enabled = False
+            Label17.Enabled = False
+            Label16.Enabled = False
+
+        End If
+    End Sub
+End Class
